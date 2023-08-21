6a0aa4e98bdd4558904519ace0e900ec3a29a93d
Ivan <86149395+Aradjj90@users.noreply.github.com>
2023-08-21 21:58:44 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+# database connection
+host = "127.0.0.1"
+user = "user name"
+password = "your name"
+db_name = "time_working"
+
+
+# name and code of the label ref
+workers = {'1': ['Ivan', '66 2E A 1F', ''],
+           '2': ['Діма', '41 A2 F2 C3', '67 61 48 B2'],
+           '3': ['Света', '56 A1 F2 D3', '17 18 G8 B6']
+}
